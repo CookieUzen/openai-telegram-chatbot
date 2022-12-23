45d370c3e2ec668c7cd9b96fd326bd0b5fe1f401
CookieUzen <uzen.huang@gmail.com>
2022-12-22 21:18:57 -0600

    added /img

--- bot.py
+++ bot.py
@@ -105,6 +105,21 @@ def ill(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text=f"Ill value set to {ill}")
 
 
+# for /img command
+def img(update: Update, context: CallbackContext):
+    prompt = " ".join(update.message.text.split(" ")[1:])
+
+    responce = openai.Image.create(
+        prompt=prompt,
+        size="256x256",
+        response_format="url",
+        n=1
+    )
+
+    print(responce)
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=responce['data'][0]['url'])
+
+
 echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
 dispatcher.add_handler(echo_handler)
 continue_handler = CommandHandler('continue', continue_)
@@ -113,5 +128,7 @@ ill_handler = CommandHandler('ill', ill)
 dispatcher.add_handler(ill_handler)
 chat_handler = CommandHandler('chat', chat)
 dispatcher.add_handler(chat_handler)
+img_handler = CommandHandler('img', img)
+dispatcher.add_handler(img_handler)
 
 updater.start_polling()
